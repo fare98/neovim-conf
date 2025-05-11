1de2e02627bea7c6a12bc0c9d64d84c55ca5edbc
Faruk Imamovic <farukimamovic@Faruks-MacBook-Pro.local>
2025-05-11 20:13:35 +0200

    inital commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,26 @@
+return {
+
+    'nvim-treesitter/nvim-treesitter',
+    build = ":TSUpdate",
+    config = function()
+	local configs = require("nvim-treesitter.configs")
+	configs.setup({
+	    highlight = {
+		enable = true,
+	    },
+	    indent = { enable = true },
+	    autotage = { enable = true },
+	    ensure_installed = {
+		"lua", 
+		"tsx",
+		"typescript",
+		"php",
+		"vim",
+		"markdown",
+		"c",
+	    },
+	    auto_install = false,
+	})
+    end
+}
+
